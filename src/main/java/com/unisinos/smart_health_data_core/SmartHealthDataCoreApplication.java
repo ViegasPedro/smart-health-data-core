afd5a05dd910030db18b62d315edc976e46301f9
Pedro Viegas <pedro.viegas@totvs.com.br>
2024-09-28 17:52:12 -0300

    first commit

---
+++ src/main/java/com/unisinos/smart_health_data_core/SmartHealthDataCoreApplication.java
@@ -0,0 +1,13 @@
+package com.unisinos.smart_health_data_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHealthDataCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHealthDataCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unisinos/smart_health_data_core/SmartHealthDataCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unisinos.smart_health_data_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHealthDataCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
